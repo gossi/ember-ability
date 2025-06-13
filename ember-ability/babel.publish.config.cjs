5f04261c503b6cfd47d1593f7b02497bc24857d3
gossi <gossi@users.noreply.github.com>
2025-06-13 16:17:27 +0200

    Switch to `@embroider/try`

---
+++ ember-ability/babel.publish.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  plugins: [
+    [
+      '@babel/plugin-transform-typescript',
+      {
+        allExtensions: true,
+        allowDeclareFields: true,
+        onlyRemoveTypeImports: true
+      }
+    ],
+    [
+      'babel-plugin-ember-template-compilation',
+      {
+        targetFormat: 'hbs',
+        transforms: []
+      }
+    ],
+    [
+      'module:decorator-transforms',
+      {
+        runtime: {
+          import: 'decorator-transforms/runtime-esm'
+        }
+      }
+    ]
+  ],
+
+  generatorOpts: {
+    compact: false
+  }
+};
