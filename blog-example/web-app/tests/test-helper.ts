fa5330d98ff8bf870ea58d79276aa04815d9188a
gossi <gossi@users.noreply.github.com>
2025-06-26 16:49:03 +0200

    Revert "try different msw setup"
    This reverts commit 3ee8cad977e88be27c8a9a962d44b78e3d674e3f.

--- blog-example/web-app/tests/test-helper.ts
+++ blog-example/web-app/tests/test-helper.ts
@@ -4,10 +4,7 @@ import { setup } from 'qunit-dom';
 import { setupEmberOnerrorValidation, start as qunitStart } from 'ember-qunit';
 
 import Application from '#/app.ts';
-import { handlers } from '#/mocks/handlers';
 import config, { enterTestMode } from '#config';
-
-import { initMSW } from './helpers';
 
 export function start() {
   enterTestMode();
@@ -16,7 +13,6 @@ export function start() {
   // eslint-disable-next-line import-x/namespace
   setup(QUnit.assert);
   setupEmberOnerrorValidation();
-  initMSW(QUnit, handlers);
 
   qunitStart();
 }
